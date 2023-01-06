32f29d014a30e091a38384b0aaa2a284dfe855c7
Tomi <horvat.tomi1@gmail.com>
2023-01-06 11:45:50 +0100

    updated aboutMe

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,12 +7,7 @@ export default function AboutMe() {
       <h2>About me</h2>
       <p>
         My name is Tomi Horvat. I'm 25 years old and come from Maribor. I
-        graduated on Faculty of Education as a kindergarten teacher.
-      </p>
-      <br></br>
-      <p>
-        Already before finishing my diploma, I doubted that I made the right
-        choice of my education. But I promised myself that I would try. When I
+        graduated on Faculty of Education as a kindergarten teacher. When I
         finished working in the kindergarten, it was clear to me that I would
         rather work in some other field. I chose programming because I saw the
         current trend of "self-taught programmers" and wanted to give it a go.
